fb28016346683a9ed0f9f22a08f3edbf82f859bb
Abhishek Raj <abhishekraj1130@gmail.com>
2022-07-23 21:08:52 +0530

    init

---
+++ src/test/java/com/abhishekraj1130/springbootblogrestapi/SpringbootBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekraj1130.springbootblogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
